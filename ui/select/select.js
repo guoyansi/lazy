af5ac5ca3eed1e09b8e672e46808476f998f581f
guoys <2835127910@qq.com>
2018-10-16 11:18:08 +0800

    修改select.js

--- ui/select/select.js
+++ ui/select/select.js
@@ -1,14 +1,14 @@
 (function($){
-	var list=[];
+	var stopMeth=function(){};
+	var checkedMeth=function(){};
+	var startMeth=function(){};
 	//生成option  html
 	var optionHtml=function(obj){
+		var list=obj.data("list");
 		var len=list.length;
 		var html="";
 		var searchStr=$(".ui-select-slide-search").val();
 		searchStr?"":searchStr="";
-		//var values=obj.attr("values");
-		//values=values?values.split(","):[];
-		//var len1=values.length;
 		var checked="";
 		var display="";
 		for(var i=0;i<len;i++){
@@ -24,13 +24,16 @@
 				display="display:none";
 			}
 			html+="<p class='ui-select-slide-con-item' style='"+display+"'>";
-			html+="<input class='ui-select-slide-checkbox' type='checkbox' "+checked+" value='"+list[i].value+"' txt='"+list[i].txt+"' />"+list[i].txt;
+			html+="<input class='ui-select-slide-checkbox' type='checkbox' "+checked+" value='"+list[i].value+"' />"+list[i].txt;
 			html+="</p>";		
 		}
 		return html;
 	}
 	//单击选项
-	var clickOption=function(obj,targetValue,opts){
+	var clickOption=function(obj,targetValue){
+		/*if(!enableEmpty&&$(".ui-select-slide-checkbox:checked").length<=1){//不允许为空
+			return;
+		}*/
 		if(targetValue=="all"){//单击的是所有
 			if($(".ui-select-slide-checkbox[value='all']").is(":checked")){
 				$(".ui-select-slide-checkbox").prop("checked",true);
@@ -38,39 +41,60 @@
 				$(".ui-select-slide-checkbox").prop("checked",false);
 			}
 		}else{//单机的是其他
-			if($(".ui-select-slide-checkbox:not(:checked)").length>0){
+			if($(".ui-select-slide-checkbox:not(:checked)").not("[value=all]").length>0){
 				$(".ui-select-slide-checkbox[value='all']").prop("checked",false);
+			}else{
+				$(".ui-select-slide-checkbox[value='all']").prop("checked",true);
 			}
 		}
-		var values=[];
+		changeList(obj);
+		inputConChange(obj);
+	};
+	//填充文本框
+	var inputConChange=function(obj){
+		var list=obj.data("list");
 		var txts=[];
+		var len=list.length;
+		if(list[0].value=="all"&&list[0].checked){
+			txts=[list[0].txt];
+		}else{
+			for(var i=0;i<len;i++){
+				if(list[i].checked){
+					txts.push(list[i].txt);
+				}
+			}
+		}
+		obj.val(txts.join(","));
+	};
+	//更新list
+	var changeList=function(obj){
+		var values=[];
 		$(".ui-select-slide-checkbox:checked").each(function(index){
-			var checkSelf=$(this);
-			txts.push(checkSelf.attr("txt"));
-			values.push(checkSelf.val());
+			values.push($(this).val());
 		});
-		obj.val(txts.join(",")).attr("values",values.join(","));
-		var len=opts.list.length;
+		var list=obj.data("list");
+		var len=list.length;
 		var len1=values.length;
 		for(var i=0;i<len;i++){
-			opts.list[i].checked=false;
+			list[i].checked=false;
 			for(var j=0;j<len1;j++){
-				if(opts.list[i].value==values[j]){
-					opts.list[i].checked=true;
+				if(list[i].value==values[j]){
+					list[i].checked=true;
 					break;
 				}
 			}
 		}
+		obj.data("list",list);
 	};
 	
-	var slideHtml=function(obj,opts){
+	var slideHtml=function(obj,search,zIndex,enableEmpty){
 		var offset=obj.offset();
 		var left=offset.left;
 		var top=offset.top;
 		var h=obj.outerHeight();
 		var html="";
-		html+="<div class='ui-select-slide-box' style='left:"+left+"px;top:"+(top+h+1)+"px;z-index:"+opts.zIndex+"'>";
-		if(opts.search){
+		html+="<div class='ui-select-slide-box' style='left:"+left+"px;top:"+(top+h+1)+"px;z-index:"+zIndex+";width:"+obj.outerWidth()+"px'>";
+		if(search){
 			html+="<div class='ui-select-slide-search-box'>";
 			html+="<input class='ui-select-slide-search' placeholder='模糊搜索' />";
 			html+="</div>";
@@ -80,16 +104,19 @@
 		html+="</div>";
 		html+="</div>";
 		$("body").append(html);
-		
+		/*if($(".ui-select-slide-checkbox:not(:checked)").not("[value=all]").length==0){
+			$(".ui-select-slide-checkbox[value='all']").prop("checked",true);
+		}*/
+		$(".ui-select-slide-box").data("target",obj);
 		$(".ui-select-slide-box").click(function(e){
 			e.stopPropagation();
 		}).on("click",".ui-select-slide-con-item",function(){
 			var self=$(".ui-select-slide-checkbox",this);
 			var checked=self.prop("checked");
 			self.prop("checked",!checked);
-			clickOption(obj,self.val(),opts);
+			clickOption(obj,self.val(),enableEmpty);
 		}).on("click",".ui-select-slide-checkbox",function(e){
-			clickOption(obj,$(this).val(),opts);
+			clickOption(obj,$(this).val(),enableEmpty);
 			e.stopPropagation();
 		});
 		$(".ui-select-slide-search").keyup(function(){
@@ -98,102 +125,130 @@
 	}
 	
 	$(document).click(function(e){
-		$(".ui-select-slide-box").remove();
+		//$(".ui-select-slide-box").remove();
+		stopChecked();
 	});
 	$(window).resize(function(){
-		$(".ui-select-slide-box").remove();
+		//$(".ui-select-slide-box").remove();
+		stopChecked();
 	});
 	$.fn.ui_get_select=function(type){
-		var vs=this.attr("values");
-		vs=vs?vs.split(","):[];
-		var ts=this.val();
-		ts=ts?ts.split(","):[];
-		if(vs.length>0&&vs[0]=="all"){
-			vs.shift();
-			ts.shift();
+		var list=this.data("list");
+		var len=list.length;
+		var isAll=false;
+		var i=0;
+		if(list[0].value=="all"&&list[0].checked){
+			isAll=true;
+			i=1;
 		}
-		if(type=="value"){
-			return vs;
-		}else if(type=="txt"){
-			return ts;
-		}else{
-			var len=vs.length;
-			var res=[];
-			for(var i=0;i<len;i++){
-				res.push([vs[i],ts[i]]);
+		var res=[];
+		for(;i<len;i++){
+			if(!list[i].checked){
+				continue;
 			}
-			return res;
+			if(type=="value"){
+				res.push(list[i].value);
+			}else if(type=="txt"){
+				res.push(list[i].txt);
+			}else{
+				res.push([list[i].value,list[i].txt]);
+			}
+		
 		}
+		return {isAll:isAll,data:res};
 	};
+	//如果是select标签
+	var ifSelectTag=function(obj,all){
+		var list=[];
+		if(all){
+			list=[{value:"all",txt:"全部"}];
+		}
+		var selected=false;
+		$("option",obj).each(function(){
+			var self=$(this);
+			list.push({value:self.val(),txt:self.html(),checked:self.attr("selected")?true:false});
+		});
+		var clsName=obj.attr("class");
+		var idName=obj.attr("id");
+		var name=obj.attr("name");
+		var newObj=$("<input id='"+(idName?idName:'')+"' class='"+(clsName?clsName:'')+"' name='"+(name?name:'')+"' />");
+		obj.replaceWith(newObj);
+		newObj.data("list",list);
+		return newObj;
+	}
+	
+	var stopChecked=function(){
+		var box=$(".ui-select-slide-box");
+		if(box.length>0){
+			box.remove();
+			stopMeth();	
+		}
+	}
+	var startAllEmentChecked=function(list){
+		for(var i=0;i<list.length;i++){
+			if(!list[i].checked){
+				return {value:"all",txt:"全选",checked:false};
+			}
+		}
+		return {value:"all",txt:"全选",checked:true};
+	}
+	
 	$.fn.ui_select=function(opts){
 		var defaults={
-			all:false,
-			search:true,
+			all:false,//是有有全选项
+			search:true,//是否有搜索框
 			zIndex:100,
+			enableEmpty:false,//是否允许为空
+			start:function(){},
 			checked:function(){//选中回调
 				
 			},
 			stop:function(){//选完回调
 				
 			},
-			list:[{value:1,txt:"测试1",checked:true},{value:2,txt:"测试2"},{value:3,txt:"测试3"},{value:4,txt:"测试4"}]
+			list:[{value:1,txt:"默认选中",checked:true},{value:2,txt:"测试2"},{value:3,txt:"测试3"},{value:4,txt:"测试4"}]
 		};
-		this.attr("readonly","readonly");
 		return this.each(function(index){
 			var obj=$(this);
 			var tagName=obj[0].tagName.toLowerCase();
 			opts=$.extend({},defaults,opts);
+			if(opts.all){
+				opts.list.unshift(startAllEmentChecked(opts.list));
+			}
+			if(!obj.data("list")){
+				obj.css({"padding-right":"25px","width":(obj.outerWidth()-20)+"px"});
+			}
 			if(tagName=="input"){
-				
+				obj.data("list",opts.list);
 			}else if(tagName=="select"){
-				var myList=[];
-				opts.list=[];
-				var selected=false;
-				$("option",obj).each(function(){
-					var self=$(this);
-					opts.list.push({value:self.val(),txt:self.html(),checked:self.attr("selected")?true:false});
-				});
-				var clsNmae=obj.attr("class");
-				var idName=obj.attr("id");
-				var newObj=$("<input id='"+(idName?idName:'')+"' class='"+(clsNmae?clsName:'')+"' />");
-				obj.replaceWith(newObj);
-				obj=newObj;
+				obj=ifSelectTag(obj,opts.all);
 			}else{
 				return;
 			}
-			if(obj.data("exce")){
-				return;
-			}else{
-				obj.data("exce",true);
-				obj.css({"padding-right":"25px","width":(obj.width()-20)+"px"})
-			}
-			//.after("<span class='ui-select-slide-empty-btn'>x</span>");
-			var nextObj=obj.next(".ui-select-slide-empty-btn");
-			if(nextObj.length>0){
-				
-			}else{
+			obj.attr("readonly","readonly");
+			if(opts.enableEmpty){
+				var nextObj=obj.next(".ui-select-slide-empty-btn");
+				nextObj.length>0?nextObj.remove():null;
 				var emptyBtn=$("<span class='ui-select-slide-empty-btn'>x</span>");
 				obj.after(emptyBtn);
-				emptyBtn.click(function(){
-					$(this).prev().val("").removeAttr("values");
-					for(var p=0;p<opts.list.length;p++){
-						opts.list[p].checked=false;
-					}
+				emptyBtn.click(function(e){
+					$(this).prev().val("");
+					$(".ui-select-slide-checkbox:checked").prop("checked",false);
+					changeList(obj);
+					e.stopPropagation();
 				});
 			}
+			
+			inputConChange(obj);
+			if(obj.data("clickEvent")){
+				return;
+			}
+			obj.data("clickEvent",true);
 			obj.click(function(e){
 				$(".ui-select-slide-box").remove();
-				list=opts.list;
-				if(opts.all&&!obj.data("all")){
-					list.unshift({value:"all",txt:"全选"});
-					obj.data("all",true);
-				}
-				slideHtml($(this),opts);
+				stopMeth=opts.stop;
+				slideHtml($(this),opts.search,opts.zIndex,opts.enableEmpty);
 				e.stopPropagation();
-			}).mouseenter(function(){
-				
-			}).mouseleave(function(){
-				
 			});
 		});
 	};
